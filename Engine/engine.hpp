f08cae3778f533bb36f1ebd7b34cf15425f04af2
Kirill <Kirilla-2001@mail.ru>
2023-02-18 02:12:28 +0300

    Changed project structure in `CMakeLists.txt`.

--- Engine/engine.hpp
+++ Engine/engine.hpp
@@ -4,7 +4,6 @@
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <vulkan/vulkan_raii.hpp>
-#include <shaderc/shaderc.hpp>
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
